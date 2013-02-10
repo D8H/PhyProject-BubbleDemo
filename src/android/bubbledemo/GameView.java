95208205a4fbcc519f00317616761556e86172c6
D8H <>
2013-02-10 17:54:07 +0100

    Fix license dates

--- src/android/bubbledemo/GameView.java
+++ src/android/bubbledemo/GameView.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2012 Davy Hélard
+ * Copyright 2012, 2013 Davy Hélard
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
